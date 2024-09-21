4d712ba2ec412f23a11c646cd343ce28bd8f0319
nuuuri <sbfl125@gmail.com>
2024-09-22 00:39:02 +0900

    [feat] NicknameEditForm, FollowList 추가

--- front/pages/profile.tsx
+++ front/pages/profile.tsx
@@ -1,13 +1,28 @@
 import Head from "next/head";
 import AppLayout from "../components/AppLayout";
+import NicknameEditForm from "../components/NicknameEditForm";
+import FollowList from "../components/FollowList";
 
 export default function Profile() {
+  const followingList = [
+    { nickname: "누리11" },
+    { nickname: "누리22" },
+    { nickname: "누리33" },
+  ];
+  const followerList = [
+    { nickname: "누리11" },
+    { nickname: "누리22" },
+    { nickname: "누리33" },
+  ];
+
   return (
     <AppLayout>
       <Head>
         <title>프로필 | NodeBird</title>
       </Head>
-      <div>내 프로필</div>
+      <NicknameEditForm />
+      <FollowList header="팔로잉 목록" data={followingList} />
+      <FollowList header="팔로워 목록" data={followerList} />
     </AppLayout>
   );
 }
