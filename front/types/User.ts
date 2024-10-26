367309b98a50d0b4d591aec1a6c7253057e7c99a
nuuuri <sbfl125@gmail.com>
2024-10-26 12:27:00 +0900

    [feat] User type 분리(User, Me)

--- front/types/User.ts
+++ front/types/User.ts
@@ -5,6 +5,12 @@ export interface User {
   email: string;
   password: string;
   nickname: string;
+  Posts: number;
+  Followings: number;
+  Followers: number;
+}
+
+export interface Me extends Omit<User, 'Posts' | 'Followings' | 'Followers'> {
   Posts: Post[];
   Followings: Pick<User, 'id' | 'nickname'>[];
   Followers: Pick<User, 'id' | 'nickname'>[];
