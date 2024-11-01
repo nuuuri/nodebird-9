2c4f94146f9b3307103fbd1bb2c9e1c01d2d54c2
nuuuri <sbfl125@gmail.com>
2024-11-01 19:54:00 +0900

    [feat] 배포 설정

--- back/app.js
+++ back/app.js
@@ -7,6 +7,8 @@ const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const path = require("path");
+const hpp = require("hpp");
+const helmet = require("helmet");
 
 const postRouter = require("./routes/post");
 const postsRouter = require("./routes/posts");
@@ -27,10 +29,17 @@ db.sequelize
 
 passportConfig();
 
-app.use(morgan("dev"));
+if (process.env.NODE_ENV === "production") {
+  app.use(morgan("combined"));
+  app.use(hpp());
+  app.use(helmet());
+} else {
+  app.use(morgan("dev"));
+}
+
 app.use(
   cors({
-    origin: "http://localhost:3060",
+    origin: ["http://localhost:3060", "nodebird.com"],
     credentials: true,
   })
 );
